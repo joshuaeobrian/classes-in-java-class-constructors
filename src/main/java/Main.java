f1cd05b9728bbdec5ecc2a2fed6f7e76680ce25a
Josh O'Brian <joshuaeobrian@gmail.com>
2017-03-06 10:36:22 -0500

    first Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,21 +7,24 @@
 public class Main {
 
     // todo: Create a main() method.
+	public static void main(String[] args) {
 
-        // todo: Create a new instance of the Character class named wizard. The instance's type should be set to 'wizard', but all other properties are up to you.
 
-        // todo: Create a new instance of the Character class named orc. The instance's type should be set to 'orc', but all other properties are up to you.
-
-        // todo: Print the wizard to the console.
-
-        // todo: Print the orc to the console.
-
-        // todo: Have the wizard attack the orc
-
-        // todo: Have the orc attack the wizard
-
-        // todo: Print the wizard to the console again.
-
-        // todo: Print the orc to the console again.
-
+		// todo: Create a new instance of the Character class named wizard. The instance's type should be set to 'wizard', but all other properties are up to you.
+		Character wizard = new Character("William the Wizard", "wizard",100,24);
+		// todo: Create a new instance of the Character class named orc. The instance's type should be set to 'orc', but all other properties are up to you.
+		Character orc = new Character("Oscar the Ord","orc",100,12);
+		// todo: Print the wizard to the console.
+		System.out.println(wizard);
+		// todo: Print the orc to the console.
+		System.out.println(orc);
+		// todo: Have the wizard attack the orc
+		wizard.attack(orc);
+		// todo: Have the orc attack the wizard
+		 orc.attack(wizard);
+		// todo: Print the wizard to the console again.
+		System.out.println(wizard);
+		// todo: Print the orc to the console again.
+		System.out.println(orc);
+	}
 }
